8b97d48414f8d96dcaa0790c18a5bda872edced4
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-09 00:52:19 +1000

    Update player.py

--- awesome/game/player.py
+++ awesome/game/player.py
@@ -25,23 +25,19 @@ async def info(session: CommandSession):
     name = get_nickname(session)
     player = await PlayerModel.get_player(id)
     # 向用户发送东西
-    ret = f"""
-    用户：{name}
-    功力：{player.energy}
-    境界：{player.get_level_name()}
-    """
+    ret = f"用户：{name}\n" \
+          f"功力：{player.energy}\n" \
+          f"境界：{player.get_level_name()}"
     await session.send(ret)
     # 等待输入内容
     more = (await session.aget(prompt='输入「更多」以查看其他数据')).strip()
     if more is "更多":
-        ret = f"""
-        用户：{name}
-        血量：{player.hp}
-        攻击力：{player.atk}
-        防御力：{player.defence}
-        闪避率：{player.doge}
-        暴击率：{player.crit}
-        韧性值：{player.res}
-        暴击伤害：{player.critDmg}
-        """
+        ret = f"用户：{name}\n" \
+              f"血量：{player.hp}\n" \
+              f"攻击力：{player.atk}\n" \
+              f"防御力：{player.defence}\n" \
+              f"闪避率：{player.doge}\n" \
+              f"暴击率：{player.crit}\n" \
+              f"韧性值：{player.res}\n" \
+              f"暴击伤害：{player.critDmg}"
         await session.send(ret)
