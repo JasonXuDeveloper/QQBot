a9d881ddba1614904b709196697af49fa9b6b718
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-09 00:12:32 +1000

    Update playerModel.py

--- models/playerModel.py
+++ models/playerModel.py
@@ -1,13 +1,12 @@
 class PlayerModel:
-    id = -1  # QQ号
-    _atk = 1  # 攻击力
-    _hp = 1  # 血量
-    _def = 0  # 防御力
-    _doge = 0  # 闪避
-    _hit = 0  # 命中
-    _crit = 0  # 暴击
-    _res = 0  # 韧性
-    _critDmg = 150  # 暴击伤害百分比
-
     def __init__(self, id: int):
         self.id = id
+        self.id = -1  # QQ号
+        self._atk = 1  # 攻击力
+        self._hp = 1  # 血量
+        self._def = 0  # 防御力
+        self._doge = 0  # 闪避
+        self._hit = 0  # 命中
+        self._crit = 0  # 暴击
+        self._res = 0  # 韧性
+        self._critDmg = 150  # 暴击伤害百分比
