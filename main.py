d6635c4516fb5369187added47f0306a839a1773
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-09 01:27:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from os import path
 if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_plugins(
-        path.join(path.dirname(__file__), 'awesome', 'game'),
+        path.join(path.dirname(__file__), 'awesome', 'plugins'),
         'awesome.game'
-    )
-    nonebot.load_plugins(
-        path.join(path.dirname(__file__), 'awesome', 'basic'),
-        'awesome.basic'
     )
     nonebot.run(host='127.0.0.1', port=8080)
