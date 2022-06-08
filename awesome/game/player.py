190cecf16ae720f2fe2a29b05e12f15586914eb5
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-09 00:38:05 +1000

    update

--- awesome/game/player.py
+++ awesome/game/player.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from nonebot import on_command, CommandSession, SenderRoles
 from models.playerModel import *
 from utils.jsonHelper import encode
@@ -18,15 +20,14 @@ def get_key(id : int) -> str:
 
 # on_command 装饰器将函数声明为一个命令处理器
 # 命令名字，别名，是否需要艾特了才能触发，然后必须来自群（JEngine群921271552）才能处理
-@on_command('info', aliases=('自视', '属性'), only_to_me=False, permission=permission)
+@on_command('info', aliases=('自视', '属性'), only_to_me=False, permission=permission, run_timeout=timedelta(seconds=2))
 async def info(session: CommandSession):
     # 取得消息的内容，并且去掉首尾的空白符
     # ctx = session.current_arg_text.strip()
-    # 等待输入内容
-    # await session.aget(prompt='你想查询哪个城市的天气呢？')
 
     # 获取用户信息
     id = get_id(session)
+    name = get_nickname(session)
     key = get_key(id)
     player = await get_data_as_model_object(key)
     # 没有就注册并保存
@@ -35,4 +36,23 @@ async def info(session: CommandSession):
         # 保存
         await set_data_as_json(key, player)
     # 向用户发送东西
-    await session.send(encode(player))
+    ret = f"""
+    用户：{name}
+    功力：{player.energy}
+    境界：{player.get_level_name()}
+    """
+    await session.send(ret)
+    # 等待输入内容
+    more = (await session.aget(prompt='输入「更多」以查看其他数据')).strip()
+    if more is "更多":
+        ret = f"""
+        用户：{name}
+        血量：{player.hp}
+        攻击力：{player.atk}
+        防御力：{player.defence}
+        闪避率：{player.doge}
+        暴击率：{player.crit}
+        韧性值：{player.res}
+        暴击伤害：{player.critDmg}
+        """
+        await session.send(ret)
--- models/playerModel.py
+++ models/playerModel.py
@@ -9,3 +9,41 @@ class PlayerModel:
         self._crit = 0  # 暴击
         self._res = 0  # 韧性
         self._critDmg = 150  # 暴击伤害百分比
+        self.energy = 0  # 功力
+        self.level = 1  # 境界
+
+    def get_level_name(self) -> str:
+        """
+        获取境界名
+        :return:
+        """
+        ls = "筑基、开光、融合、心动、金丹、元婴、出窍、分神、合体、洞虚、大乘、渡劫".split('、')
+        return ls[self.level]
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @property
+    def atk(self):
+        return self._atk
+
+    @property
+    def defence(self):
+        return self._def
+
+    @property
+    def doge(self):
+        return self._doge
+
+    @property
+    def crit(self):
+        return self._crit
+
+    @property
+    def res(self):
+        return self._res
+
+    @property
+    def critDmg(self):
+        return self._critDmg
