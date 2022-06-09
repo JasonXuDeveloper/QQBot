4fd7c82489f0dc34309e30488c5a98717c6735be
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-09 10:28:10 +1000

    update

--- awesome/games/basic.py
+++ awesome/games/basic.py
@@ -24,7 +24,18 @@ async def info(session: CommandSession):
         # 可以结束
         if diff >= 300:
             # 随机功力
-            reward = get_rand_int(1,5)
+            # 10分钟内
+            if diff <= 60 * 10:
+                reward = get_rand_int(1,3)
+            # 30分钟内
+            elif diff <= 60 * 30:
+                reward = get_rand_int(2,5)
+            # 60分钟内
+            elif diff <= 60 * 60:
+                reward = get_rand_int(3,8)
+            # 60分钟以上
+            else:
+                reward = get_rand_int(5,10)
             # 更新数据
             player.energy += reward
             player.last_med = 0  # 取消打坐
@@ -44,3 +55,13 @@ async def info(session: CommandSession):
         # 向用户发送东西
         ret = f"道友「{name}」开始打坐"
         await session.send(ret)
+
+
+# 功力榜
+@on_command('energy_rank', aliases=('功力榜'), only_to_me=False, permission=permission, run_timeout=timedelta(seconds=15))
+async def info(session: CommandSession):
+    # 取得消息的内容，并且去掉首尾的空白符
+    # ctx = session.current_arg_text.strip()
+    # 向用户发送东西
+    ret = f"一会儿写..."
+    await session.send(ret)
--- awesome/plugins/basic.py
+++ awesome/plugins/basic.py
@@ -4,17 +4,9 @@ from nonebot import on_command, CommandSession
 
 @on_command('usage', aliases=['使用帮助', '帮助', '使用方法', '功能', '功能列表'], only_to_me=False)
 async def usage(session: CommandSession):
-    # 获取设置了名称的插件列表
-    plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
-
-    arg = session.current_arg_text.strip().lower()
-    if not arg:
-        # 如果用户没有发送参数，则发送功能列表
-        await session.send(
-            '我现在支持的功能有：\n' + '\n- '.join(p.name for p in plugins))
-        return
-
-    # 如果发了参数则发送相应命令的使用帮助
-    for p in plugins:
-        if p.name.lower() == arg:
-            await session.send(p.usage)
+    # 发送功能列表
+    await session.send('功能列表：\n'\
+        '- 【属性】、【自视】，查看自身属性\n'\
+        '- 【打坐】、【修炼】，开始或结束打坐，并获得功力\n'\
+        '- 【功力榜】，记载功力最高的10位道友')
+    return
